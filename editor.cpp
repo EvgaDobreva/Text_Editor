52551206d4b38c2a188ac5de82d673c730865264
evga <evg.dobrev@gmail.com>
2017-12-09 16:54:38 +0200

    Fixed Backspace and Delete.

--- editor.cpp
+++ editor.cpp
@@ -21,6 +21,10 @@ public:
 	void save();
 	void delete_line(int y);
 	void delete_char(int x, int y, int direction);
+	void insert_char(int& x, int& y, char cursor);
+	void new_line(int y);
+	void move_key_up(int& y, int& x);
+	void move_key_down(int& y, int& x);
 };
 
 FileContentBuffer::FileContentBuffer(string file) {
@@ -52,17 +56,19 @@ void FileContentBuffer::print() {
 }
 
 void FileContentBuffer::save() {
-	ofstream outfile("out.txt");
-	vector<string>::const_iterator iterator;
-	for (iterator = lines_.begin(); iterator != lines_.end(); ++iterator) {
-	   outfile << (*iterator).c_str();
+	ofstream outfile(filename_.c_str());
+	vector<string>::const_iterator iterator = lines_.begin();
+	outfile << (*iterator).c_str();
+	for (++iterator; iterator != lines_.end(); ++iterator) {
 	   outfile << "\n";
+	   outfile << (*iterator).c_str();
 	}
 	outfile.close();
 }
 
 void FileContentBuffer::delete_line(int y) {
 	clrtoeol();
+	lines_.erase(lines_.begin()+y);
 }
 
 void FileContentBuffer::delete_char(int x, int y, int direction) {
@@ -73,6 +79,52 @@ void FileContentBuffer::delete_char(int x, int y, int direction) {
 		lines_[y].erase(x, 1);
 	}
 	print();
+}
+
+void FileContentBuffer::insert_char(int& x, int& y, char cursor) {
+	if (cursor == '\n') {
+		if (x == 0) {
+			new_line(y-1);
+		}
+		else if (x == (int) lines_[y].length()) {
+			new_line(y);
+			x=0;
+		}
+		else {
+			string first = lines_[y].substr(0, x-1);
+			string second = lines_[y].substr(x, lines_[y].length() - x);
+			new_line(y);
+			lines_[y] = first;
+			lines_[y+1] = second;
+			x=0;
+		}
+		y++;
+	}
+	else {
+		lines_[y].insert(x++, 1, char(cursor));
+	}
+}
+
+void FileContentBuffer::new_line(int y) {
+	lines_.insert(lines_.begin()+y, "");
+}
+
+void FileContentBuffer::move_key_up(int& y, int& x) {
+	if (y>0) {
+		if (lines_[y-1].size() < lines_[y].size()) {
+			x=(int) lines_[y-1].length();
+		}
+		y--;
+	}
+}
+
+void FileContentBuffer::move_key_down(int& y, int& x) {
+	if (y+1 < (int) lines_.size()) {
+		if (lines_[y+1].size() < lines_[y].size()) {
+			x=(int) lines_[y+1].length();
+		}
+		y++;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -103,8 +155,7 @@ int main(int argc, char* argv[])
 				if (x-1 >= 0) {
 					x--;
 				} else if (x == 0 && y-1 >= 0) {
-					x=(int) file.lines_[y].length();
-					y--;
+					x=(int) file.lines_[--y].length();
 				}
 				break;
 			case KEY_RIGHT:
@@ -117,14 +168,13 @@ int main(int argc, char* argv[])
 				}
 				break;
 			case KEY_UP:
-				if (y>0)	y--; break;
+				file.move_key_up(y, x);
+				break;
 			case KEY_DOWN:
-				if (y+2 < (int) file.lines_.size()) {
-					y++;
-				}
+				file.move_key_down(y, x);
 				break;
 			case 4: // Ctrl+A=1, Ctrl+B=2, Ctrl+C=3, Ctrl+D=4, ...
-				file.delete_line(y+1);
+				file.delete_line(y);
 				break;
 			case KEY_BACKSPACE:
 				if (x == 0 && y > 0) {
@@ -143,10 +193,18 @@ int main(int argc, char* argv[])
 				}
 				break;
 			case KEY_DC:
-				file.delete_char(x, y, -1); break;
+				if (x == (int) file.lines_[y].size()) {
+					file.lines_[y] += file.lines_[y+1];
+					file.delete_line(y+1);
+				}
+				else {
+					file.delete_char(x, y, -1);
+				}
+				break;
 			default:
-				cout << "UNKNOWN: " << cursor << " ";
+				file.insert_char(x, y, char(cursor));
 		}
+		file.print();
 		move(y, x);
 		refresh();
 	}
@@ -155,3 +213,6 @@ int main(int argc, char* argv[])
 	endwin();
 	return 0;
 }
+/*
+	1. Change 'q' to Ctrl+q when we exit the editor.
+*/
